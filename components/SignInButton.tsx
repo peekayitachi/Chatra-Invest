d6a855b4898411614ae8ccfedd7ce936daa9a74f
anaypurohit0907 <ap1851@srmist.edu.in>
2025-03-24 19:22:57 +0530

    did auth smd

---
+++ components/SignInButton.tsx
@@ -0,0 +1,9 @@
+import { signIn } from "next-auth/react";
+
+export default function SignInButton() {
+  return (
+    <button onClick={() => signIn("google")}>
+      Sign in with Google
+    </button>
+  );
+}
